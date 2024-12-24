85a869ae8c4c3f7a10dc02d206523eab44b84f8f
d3lm3l <d3lm3l@gmail.com>
2024-12-24 18:42:50 -0500

    updated VisitedButton visited state

--- src/components/BusinessInfoWindow.tsx
+++ src/components/BusinessInfoWindow.tsx
@@ -40,14 +40,14 @@ const VisitedButton = ({
 	visited: boolean;
 	onClick: () => void;
 }) => {
-	const visitedColor = 'text-green-400';
+	const visitedColor = visited ? 'text-gray-50' : 'text-green-400';
 
 	return (
 		<motion.button
 			whileHover={{ scale: 1.02 }}
 			whileTap={{ scale: 0.95 }}
 			className={`h-[24px] flex px-3 py-4 gap-2 pointer-events-auto cursor-pointer justify-center items-center transition-all duration-150 ease-in-out bg-gray-900/50 rounded-lg outline-none hover:outline-none focus:outline-none hover:border-green-400/50 shadow-md hover:drop-shadow-lg backdrop-blur-sm text-sm text-gray-50 ${
-				visited ? '' : ''
+				visited ? 'bg-green-600/75 text-gray-50' : ''
 			}`}
 			onClick={onClick}
 		>
@@ -181,6 +181,7 @@ const BusinessInfoWindow = ({ business }: { business?: Business }) => {
 									href={business?.website}
 									target="_blank"
 									rel="noopener noreferrer"
+									title="Go to business website"
 									className="text-3xl font-bold text-white/95 hover:text-white/95 leading-tight"
 								>
 									{business.name}
