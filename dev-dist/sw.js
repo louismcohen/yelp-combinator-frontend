d4f00cbc058ffbce34dfded4fa0218042be5f590
Louis Cohen <louis@skymob.net>
2025-03-17 09:58:46 -0700

    changed theme color back to white

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f001acab'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.5l2jo9o70jo"
+    "revision": "0.t32tos920n8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
